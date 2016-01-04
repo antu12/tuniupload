af0767e7313ef776b54ef7c99991c2ccb08b4c40
Arshad Hossain <Arshad Hossain>
2016-01-04 21:22:22 +0600

    Css file added and php file structure setup

--- upload.php
+++ upload.php
@@ -1,11 +1,29 @@
+<?php
+
+ ?>
+
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <title>Upload</title>
+    <link rel="stylesheet" href="upload.css" media="screen" charset="utf-8">
     <script type="text/javascript" src="upload.js"></script>
   </head>
   <body>
+    <div class="uploaded">
 
+    </div>
+
+    <div class="upload-progress">
+
+    </div>
+
+    <div class="">
+      <form id="file-form" action="handler.php" method="POST">
+        <input type="file" id="file-select" name="photos[]" multiple/>
+        <button type="submit" id="upload-button">Upload</button>
+      </form>
+    </div>
   </body>
 </html>
